ff1547a1b7f2971cc7d06f71c973040f2ce2f715
hanhtv24 <144075133+hanhtv24@users.noreply.github.com>
2024-01-15 14:46:03 +0700

    update

--- views/teacher/complete.php
+++ views/teacher/complete.php
@@ -6,10 +6,13 @@ use app\core\Application;
 use app\core\form\TextareaField;
 use app\core\View;
 use \app\core\form\SelectionBoxField;
-
 $this->title = 'Complete Teacher';
 
 ?>
 <h3>Complete Teacher</h3>
-<h5>Bạn đã đăng kí thành công giáo viên</h5>
+<?php if (isset($method) && $method === 'edit'): ?>
+    <h5>Bạn đã sửa thành công cho giáo viên <?php echo $model->name ?></h5>
+<?php else: ?>
+    <h5>Bạn đã đăng ký thành công cho giáo viên <?php echo $model->name ?></h5>
+<?php endif; ?>
 <a href="/">Trở về trang chủ</a>
